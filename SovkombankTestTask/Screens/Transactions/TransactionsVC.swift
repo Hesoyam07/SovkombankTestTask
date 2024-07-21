30d9d83cfc4af89a7c79d595e272d3b29a00297f
Dmitriy <140389449+Hesoyam07@users.noreply.github.com>
2024-07-21 18:05:08 +0300

    Implemented TransactionsVC's UI, currency symbol converter

---
+++ SovkombankTestTask/Screens/Transactions/TransactionsVC.swift
@@ -0,0 +1,8 @@
+//
+//  TransactionsVC.swift
+//  SovkombankTestTask
+//
+//  Created by Дмитрий on 21.07.2024.
+//
+
+import Foundation
---
+++ SovkombankTestTask/Screens/Transactions/TransactionsVM.swift
@@ -0,0 +1,8 @@
+//
+//  TransactionsVM.swift
+//  SovkombankTestTask
+//
+//  Created by Дмитрий on 21.07.2024.
+//
+
+import Foundation
