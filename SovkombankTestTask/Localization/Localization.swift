f0c21d1e2b0c95fdd19ed766f310275a240aa896
Dmitriy <140389449+Hesoyam07@users.noreply.github.com>
2024-07-24 13:57:53 +0300

    Fixed localization keys

--- SovkombankTestTask/Localization/Localization.swift
+++ SovkombankTestTask/Localization/Localization.swift
@@ -18,7 +18,7 @@ struct Localization {
     static let gbp = "GBP".localized
     static let total = "Total".localized
     static let transactionFor = "TransactionsFor".localized
-    static let decodingFailureDescription = "Decoding failure".localized
-    static let dataNotLoadedFailureDescription = "Resource not found".localized
+    static let decodingFailureDescription = "DecodingFailure".localized
+    static let dataNotLoadedFailureDescription = "ResourceNotFound".localized
     static let alertOkMessage = "OK".localized
 }
